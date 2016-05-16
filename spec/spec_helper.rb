246211fa53d8025e05be9067636bec319165293e
Alex Stonehouse <alex.stonehouse@gmail.com>
2016-05-16 13:07:01 +0200

    Test coverage fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'parse4plex'
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'parse4plex'

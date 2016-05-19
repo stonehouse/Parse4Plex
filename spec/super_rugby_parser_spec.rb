b5728184f6892260b961a48918da074d554f85be
Alex Stonehouse <alex.stonehouse@gmail.com>
2016-05-19 19:01:26 +0200

    Updated test to use RSpec mocks
    SuperRugbyTest now uses RSpec mocks

--- spec/super_rugby_parser_spec.rb
+++ spec/super_rugby_parser_spec.rb
@@ -1,42 +1,31 @@
 require 'spec_helper'
-
-class SuperRugbyParserMock < Parse4Plex::SuperRugbyParser
-
-  attr_reader :glob
-
-  def initialize(fullFilePath, assets)
-    @assets = assets
-    ui = Parse4Plex::UI.new
-    super fullFilePath, ui
-  end
-
-  def assets_exist?
-    true
-  end
-
-  def list_files(globQuery)
-    @glob = globQuery
-    @assets
-  end
-
-end
+include Parse4Plex
 
 describe Parse4Plex::SuperRugbyParser do
   it 'ignores special game' do
     file = "~/Rugby/RU.2015.Super.Rugby.SemiFinal.1.x264.mp4"
-    parser = SuperRugbyParserMock.new(file, [])
+    allow(Dir).to receive(:exists?) {true}
+    ui = UI.new
+    ui.output = false
+    parser = SuperRugbyParser.new(file, ui)
     expect(parser.can_parse).not_to eq(true)
   end
 
   it 'ignores games already parsed' do
     file = "~/Rugby/Chiefs v Hurricanes - Super Rugby 2015 Round 18.mp4"
-    parser = SuperRugbyParserMock.new(file, [])
+    allow(Dir).to receive(:exists?) {true}
+    ui = UI.new
+    ui.output = false
+    parser = SuperRugbyParser.new(file, ui)
     expect(parser.can_parse).not_to eq(true)
   end
 
   it 'correctly parses game file name' do
     file = "~/Rugby/RU.2015.Super.Rugby.R18.Chiefs.v.Hurricanes.x264.mp4"
-    parser = SuperRugbyParserMock.new(file, [])
+    allow(Dir).to receive(:exists?) {true}
+    ui = UI.new
+    ui.output = false
+    parser = SuperRugbyParser.new(file, ui)
     expect(parser.can_parse).to eq(true)
     expectedName = "Chiefs v Hurricanes - Super Rugby 2015 Round 18.mp4"
     expect(parser.parse_name).to eq(expectedName)
@@ -45,10 +34,15 @@ describe Parse4Plex::SuperRugbyParser do
   it 'finds artwork for team' do
     file = "~/Rugby/RU.2015.Super.Rugby.R18.Chiefs.v.Hurricanes.x264.mp4"
     artwork = ['super_rugby_poster_chiefs.png', 'super_rugby_fanart_chiefs.png']
-    parser = SuperRugbyParserMock.new(file, artwork)
+    allow(Dir).to receive(:exists?) {true}
     # Expect second team because assets for both teams are always parsed
-    expectedName = ".plex_assets/super_rugby_*_hurricanes.*"
+    hurricanesAssetGlob = ".plex_assets/super_rugby_*_hurricanes.*"
+    allow(Dir).to receive(:glob).with(end_with(hurricanesAssetGlob)) {artwork}
+    chiefsAssetGlob = ".plex_assets/super_rugby_*_chiefs.*"
+    allow(Dir).to receive(:glob).with(end_with(chiefsAssetGlob)) {artwork}
+    ui = UI.new
+    ui.output = false
+    parser = SuperRugbyParser.new(file, ui)
     parser.do_parse
-    expect(parser.glob).to end_with(expectedName)
   end
 end
